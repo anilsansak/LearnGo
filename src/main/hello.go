57da3bf1b84cec3add6937b1af4c9e5c2cbae457
anilsansak <anilsansak@gmail.com>
2017-07-10 16:17:20 +0300

    Added nested loops and switch statement

--- src/main/hello.go
+++ src/main/hello.go
@@ -1,13 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	name := "anıl"
-	fmt.Printf("HELLO WORLD BABUŞ\n")
-	fmt.Printf("My name is " + name)
+	fmt.Println("HELLO WORLD BABUŞ")
+	fmt.Println("My name is " + name)
 
 	isTrue := true
-	fmt.Println("\n is it true?", isTrue)
+	fmt.Println("Is it true?", isTrue)
 
+	age := 22
+	fmt.Println("I am ", age, "years old ")
+
+	for i := 0; i <= 5; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Printf("*")
+		}
+		fmt.Printf("\n")
+	}
+
+	t := time.Now()
+
+	switch t.Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's weekend!")
+	default:
+		fmt.Println("It's weekday")
+	}
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	case t.Hour() > 12:
+		fmt.Println("It's after noon")
+	}
 }
